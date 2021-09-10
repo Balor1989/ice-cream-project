561029d8eb778f56c95d3cc1534c24c8e353d512
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-09-10 12:19:35 +0300

    Добавляет заготовки модальных окон

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-location]'),
-    closeModalBtn: document.querySelector('[data-modal-close-location]'),
-    modal: document.querySelector('[data-modal-location]'),
+    openModalBtn: document.querySelector('[data-modal-open-franchise]'),
+    closeModalBtn: document.querySelector('[data-modal-close-franchise]'),
+    modal: document.querySelector('[data-modal-franchise]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
