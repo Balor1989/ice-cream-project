52720e92f13b664e0b9f3046e400bfbb434c4b9d
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-09-13 23:03:13 +0300

    Фиксит размеры хедера

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -12,7 +12,7 @@
       const scrollLockMethod = !isMenuOpen
         ? 'disableBodyScroll'
         : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+      // bodyScrollLock[scrollLockMethod](document.body);
     };
 
     openMenuBtn.addEventListener('click', toggleMenu);
